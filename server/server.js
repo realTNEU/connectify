414de8d67ce88cef05ec839cb7761744549cc213
Ameya Taneja <112568247+realTNEU@users.noreply.github.com>
2024-10-07 13:58:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -67,5 +67,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://connectify-backend-t9yi.onrender.com:${PORT}`);
 });
